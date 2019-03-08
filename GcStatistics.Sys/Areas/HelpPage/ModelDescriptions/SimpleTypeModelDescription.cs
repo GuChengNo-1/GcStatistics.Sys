fd4427833bcdb6613ee6a4c7941c633c8f2a0c9b
朱亚涛 <40811911+veryCoolFirst@users.noreply.github.com>
2019-03-08 11:19:46 +0800

    添加项目文件。

---
+++ GcStatistics.Sys/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
@@ -0,0 +1,6 @@
+namespace GcStatistics.Sys.Areas.HelpPage.ModelDescriptions
+{
+    public class SimpleTypeModelDescription : ModelDescription
+    {
+    }
+}
---
+++ GcStatistics.Sys/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="GcStatistics.Sys.WebApiApplication" Language="C#" %>
