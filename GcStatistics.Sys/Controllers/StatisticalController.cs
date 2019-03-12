a84fdfe61fc51cc11551c91345b6416f1201ce16
zly98 <45872378+dlyf1229@users.noreply.github.com>
2019-03-12 15:40:41 +0800

    临时调试
    临时调试

--- GcStatistics.Sys/Controllers/StatisticalController.cs
+++ GcStatistics.Sys/Controllers/StatisticalController.cs
@@ -2,6 +2,7 @@
 using GcStatistics.Sys.Models;
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
@@ -18,39 +19,117 @@ namespace GcStatistics.Sys.Controllers
         WorkOfUnit work = new WorkOfUnit();
         public void Get(string key)
         {
+            //搜索引擎来源
+            var x = System.Web.HttpContext.Current.Request.UserAgent;
+            ////获取浏览器信息
+            //string aaa = Browser;
+            //火狐浏览器
+            if (x.Contains("Firefox")) { x = "火狐浏览器"; }
+            //谷歌浏览器
+            if (x.Contains("Chrome")) { x = "谷歌浏览器"; }
+            //Safari浏览器（苹果浏览器）
+            if (x.Contains("Version")) { x = "苹果浏览器"; }
+            //Opera浏览器
+            if (x.Contains("Opera")) { x = "Opera浏览器"; }
+            //LBBROWSER浏览器（猎豹）
+            if (x.Contains("LBBROWSER")) { x = "猎豹浏览器"; }
+            //sougou浏览器（sougou）
+            if (x.Contains("MetaSr")) { x = "搜狗浏览器"; }
+            //Maxthon浏览器（傲游）
+            if (x.Contains("Maxthon")) { x = "傲游浏览器"; }
+            ////获取用户使用设备
+            //string Client = "";
+            //string u = System.Web.HttpContext.Current.Request.ServerVariables["HTTP_USER_AGENT"];
+            //if (u.Contains("ipad"))
+            //{
+            //    Client = "IPAD端访问量";
+            //}
+            //else if (u.Contains("iphone os") || u.Contains("midp") || u.Contains("rv:1.2.3.4") || u.Contains("ucweb") || u.Contains("android") || u.Contains("windows ce") || u.Contains("windows mobile"))
+            //{
+            //    Client = "IPHONE端访问量";
+            //}
+            //else
+            //{
+            //    Client = "PC端访问量";
+            //}
             WebInfo web = new WebInfo();
             if (work.CreateRepository<WebInfo>().GetList(m => m.WebKey == key).Count() > 0)
             {
                 web = work.CreateRepository<WebInfo>().GetFirst(m => m.WebKey == key);
                 web.WebPv = web.WebPv + 1;
-                
             }
             VisitorInfo vist = new VisitorInfo();
-            vist.AccessTime = DateTime.Now.Date;
-            vist.VisitPage = System.Web.HttpContext.Current.Request.Url.AbsoluteUri.ToString();
+            vist.AccessTime = DateTime.Now;
+            //vist.VisitPage = System.Web.HttpContext.Current.Request.Url.AbsoluteUri.ToString();
             vist.IpAddress = "";
             vist.VisitSE = "";
             vist.WebInfo = web;
+            HttpContext.Current.Session["PageNumber"] = work.CreateRepository<WebInfo>().GetEntityById(vist.Id);
         }
+
+        //[HttpPost]
+        //#region
+        //public IEnumerable<string> Get(string key, string VisitPage, string IpAddress, string Address)
+        //{
+        //    //搜索引擎来源
+        //    var x = System.Web.HttpContext.Current.Request.UserAgent;
+        //    //火狐浏览器
+        //    if (x.Contains("Firefox")) { x = "火狐浏览器"; }
+        //    //谷歌浏览器
+        //    if (x.Contains("Chrome")) { x = "谷歌浏览器"; }
+        //    //Safari浏览器（苹果浏览器）
+        //    if (x.Contains("Version")) { x = "苹果浏览器"; }
+        //    //Opera浏览器
+        //    if (x.Contains("Opera")) { x = "Opera浏览器"; }
+        //    //LBBROWSER浏览器（猎豹）
+        //    if (x.Contains("LBBROWSER")) { x = "猎豹浏览器"; }
+        //    //sougou浏览器（sougou）
+        //    if (x.Contains("MetaSr")) { x = "搜狗浏览器"; }
+        //    //Maxthon浏览器（傲游）
+        //    if (x.Contains("Maxthon")) { x = "傲游浏览器"; }
+        //    //获取用户使用设备
+        //    string Client = "";
+        //    string u = System.Web.HttpContext.Current.Request.ServerVariables["HTTP_USER_AGENT"];
+        //    if (u.Contains("ipad"))
+        //    {
+        //        Client = "IPAD端访问量";
+        //    }
+        //    else if (u.Contains("iphone os") || u.Contains("midp") || u.Contains("rv:1.2.3.4") || u.Contains("ucweb") || u.Contains("android") || u.Contains("windows ce") || u.Contains("windows mobile"))
+        //    {
+        //        Client = "IPHONE端访问量";
+        //    }
+        //    else
+        //    {
+        //        Client = "PC端访问量";
+        //    }
+        //    WebInfo web = new WebInfo();
+        //    if (work.CreateRepository<WebInfo>().GetList(m => m.WebKey == key).Count() > 0)
+        //    {
+        //        web = work.CreateRepository<WebInfo>().GetFirst(m => m.WebKey == key);
+        //        web.WebPv = web.WebPv + 1;
+        //        web.WebUv = web.WebUv + 1;
+        //    }
+        //    VisitorInfo vist = new VisitorInfo();
+        //    vist.AccessTime = DateTime.Now;
+        //    vist.VisitPage = VisitPage;
+        //    vist.IpAddress = IpAddress;
+        //    vist.VisitSE = x;
+        //    vist.WebInfo = web;
+
+        //    return new string[] { "pv:" + web.WebPv + "-" + "uv:" + web.WebUv };
+        //}
+        //#endregion
+
         public void Put(string key)
         {
-            //string strPath = "http://" + Request.ServerVariables["HTTP_HOST"] + Request.ServerVariables["PATH_INFO"] + "?" + Request.ServerVariables["QUERY_STRING"];
-            //if (strPath.EndsWith("?"))
-            //{
-            //    strPath = strPath.Substring(0, strPath.Length - 1);
-            //}
-           string url=  HttpContext.Current.Request.Url.PathAndQuery;
-        }
-        public void Get(string key)
-        {
-
+            string url = HttpContext.Current.Request.Url.PathAndQuery;
         }
         public void Delete(string key)
         {
         }
         public void Update(string key)
         {
-            
+
         }
     }
 }
--- GcStatistics.Sys/Global.asax.cs
+++ GcStatistics.Sys/Global.asax.cs
@@ -1,4 +1,5 @@
 ﻿using GcStatistics.Sys.Dal;
+using GcStatistics.Sys.Models;
 using System;
 using System.Collections.Generic;
 using System.Data.Entity;
@@ -8,11 +9,13 @@ using System.Web.Http;
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
+using System.Web.SessionState;
 
 namespace GcStatistics.Sys
 {
     public class WebApiApplication : System.Web.HttpApplication
     {
+        WorkOfUnit work = new WorkOfUnit();
         protected void Application_Start()
         {
             AreaRegistration.RegisterAllAreas();
@@ -21,6 +24,33 @@ namespace GcStatistics.Sys
             //Database.SetInitializer(new DropCreateDatabaseIfModelChanges<GcSiteDb>());
             //当models发生改变时修改数据库
             Database.SetInitializer(new MigrateDatabaseToLatestVersion<GcSiteDb, GcStatistics.Sys.Dal.Migrations.Configuration>());
+        }
+        protected void Application_End()
+        {
+            //获取当前时间（结束时间）
+            DateTime EndTime = DateTime.Now;
+            //修改表中的结束时间
+            VisitorInfo visitor = new VisitorInfo();
+            //接受后台传递的Id
+            string id = HttpContext.Current.Session["PageNumber"].ToString();
+            work.CreateRepository<VisitorInfo>().Update(visitor, id);
+
+            //查询访客的浏览开始时间
+            int a = work.ExecuteNonQuery(string.Format("select * from VisitorInfo where Id={0}", id));
+
+
+
+        }
+        public override void Init()
+        {
+            PostAuthenticateRequest += MvcApplication_PostAuthenticateRequest;
+            base.Init();
+        }
+
+        void MvcApplication_PostAuthenticateRequest(object sender, EventArgs e)
+        {
+            HttpContext.Current.SetSessionStateBehavior(
+                SessionStateBehavior.Required);
         }
     }
 }
